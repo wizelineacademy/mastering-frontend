65bbe6f2cadae8c3e2a55fed1ed6ab38c5ab1e9c
Eder Diaz <ederchrono@gmail.com>
2018-10-07 00:42:02 -0500

    🐛 fix bug when codepen loads from the beginning

--- slides-base-project/src/components/Codepen.js
+++ slides-base-project/src/components/Codepen.js
@@ -1,4 +1,6 @@
 import React from "react";
+import styled from "styled-components";
+import Logo from "./CodepenLogo.svg";
 
 // this needs to be improved, it might only work in certain screen sizes
 const codepenStyle = {
@@ -9,9 +11,31 @@ const codepenStyle = {
   marginLeft: "-639px"
 };
 
+const logoStyle = {
+  width: "200px",
+  display: "block"
+};
+
+const LoadButton = styled.button([], {
+  backgroundColor: "purple",
+  color: "white",
+  fontSize: "30px"
+});
+
 export default class Counter extends React.Component {
+  state = { loadDemo: false };
+  handleClick = () => {
+    this.setState({ loadDemo: true });
+  };
   render() {
     const { url } = this.props;
+    if (!this.state.loadDemo) {
+      return (
+        <LoadButton onClick={this.handleClick}>
+          Load <Logo style={logoStyle} /> Demo
+        </LoadButton>
+      );
+    }
     return <iframe src={url} style={codepenStyle} />;
   }
 }

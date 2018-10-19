cc98eb262654345a4208ce45763de0a76bfffa5c
Victor Tortolero <victor.tortolero@wizeline.com>
2018-10-19 16:31:45 -0500

    Add base setup for 4th session slides

--- slides/004_responsive-design/slides.js
+++ slides/004_responsive-design/slides.js
@@ -1,8 +1,15 @@
 export { default as theme } from '../src/wizeline-theme'
+
 import intro from './intro.mdx'
+import mediaQueries from './mediaQueries.mdx'
 import patterns from './patterns.mdx'
+import units from './units.mdx'
+import cssInTheWild from './cssInTheWild.mdx'
 
 export default [
   ...intro,
-  ...patterns
+  ...mediaQueries,
+  ...patterns,
+  ...units,
+  ...cssInTheWild,
 ]

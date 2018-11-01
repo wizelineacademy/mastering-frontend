fea1469c25df72bc7729914947cf0a0d572df422
Eder Diaz <ederchrono@gmail.com>
2018-10-31 19:26:34 -0600

    📝 add session 009 slides

--- slides/009_js-dom/slides.js
+++ slides/009_js-dom/slides.js
@@ -1,6 +1,9 @@
 export { default as theme } from "../src/wizeline-theme";
 
 import intro from "./intro.mdx";
+import dom from "./dom.mdx";
+import events from "./events.mdx";
+import animations from "./animations.mdx";
 import thanks from "./thanks.mdx";
 
-export default [...intro, ...thanks];
+export default [...intro, ...dom, ...events, ...animations, ...thanks];

27af2b4051f6dde105779446b933a646a12010af
Eder Diaz <ederchrono@gmail.com>
2018-11-01 13:37:38 -0600

    ✨ add events and goodpractices slides

--- slides/009_js-dom/slides.js
+++ slides/009_js-dom/slides.js
@@ -4,6 +4,14 @@ import intro from "./intro.mdx";
 import dom from "./dom.mdx";
 import events from "./events.mdx";
 import animations from "./animations.mdx";
+import goodpractices from "./goodpractices.mdx";
 import thanks from "./thanks.mdx";
 
-export default [...intro, ...dom, ...events, ...animations, ...thanks];
+export default [
+  ...intro,
+  ...dom,
+  ...events,
+  ...goodpractices,
+  ...animations,
+  ...thanks
+];

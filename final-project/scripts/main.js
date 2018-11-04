d44e4505f4fc704cfde9f5b0460cd893dff25671
Daniel Francisco Campos Toledo <danielcampos_t@hotmail.com>
2018-11-03 22:15:10 -0600

    Added the styles for the small tablet device

--- final-project/scripts/main.js
+++ final-project/scripts/main.js
@@ -4,12 +4,3 @@ import Faq from './faq';
 
 Blog.initSlider();
 Faq.init();
-
-// testing that `const` and arrow functions transpile correctly
-const before = "Hello -world!";
-const after = before
-  .split("")
-  .filter(char => char !== "-")
-  .join("");
-console.log({ before });
-console.log({ after });

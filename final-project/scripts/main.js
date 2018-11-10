096748036b96510ed63190bad17402797bfac3b2
Adrian Toscano <adrian.toscano@wizeline.com>
2018-11-10 09:04:27 -0600

    Add blog html structure

--- final-project/scripts/main.js
+++ final-project/scripts/main.js
@@ -1,10 +1,10 @@
 import "../styles/main.css";
 
-// testing that `const` and arrow functions transpile correctly
-const before = "Hello -world!";
-const after = before
-  .split("")
-  .filter(char => char !== "-")
-  .join("");
-console.log({ before });
-console.log({ after });
+const $ = (selector) => {
+  return document.querySelector(selector)
+}
+const $$ = (selector) => {
+  return document.querySelectorAll(selector)
+}
+
+

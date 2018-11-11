cbd052582dc423ef97738daea1da64b66242d2a5
Adrian Toscano <adriantoscanop@gmail.com>
2018-11-10 21:23:08 -0600

    Add Slider func attempt

--- final-project/scripts/main.js
+++ final-project/scripts/main.js
@@ -7,4 +7,27 @@ const $$ = (selector) => {
   return document.querySelectorAll(selector)
 }
 
+const imgElement = $('.blog .carousel-container .carousel .carousel-images')
 
+const imgUrls = [
+  '../reference/assets/blog/image_1.jpg',
+  '../reference/assets/blog/image_2.jpg',
+  '../reference/assets/blog/image_3.jpg'
+]
+let currImgIndex = 0;
+
+function changeImg() {  
+  imgElement.style.backgroundImage = `url(\'${imgUrls[currImgIndex]}\')`
+}
+
+function prevImg() {
+  console.log('prev button')
+  currImgIndex--;
+  changeImg()
+}
+
+function nextImg() {
+  console.log('next button')
+  currImgIndex++;
+  changeImg()
+}

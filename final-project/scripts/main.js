82310892cb6301e11685b78e1a98c807e608efff
Leonardo Urzua <leonardo.urzua@wizeline.com>
2018-11-19 16:42:29 -0600

    add Desktop support

--- final-project/scripts/main.js
+++ final-project/scripts/main.js
@@ -3,6 +3,7 @@ import "../styles/top-nav.scss"
 import "../styles/mobile.scss"
 import "../styles/portrait.scss"
 import "../styles/landscape.scss"
+import "../styles/desktop.scss"
 import './slides'
 
 // testing that `const` and arrow functions transpile correctly

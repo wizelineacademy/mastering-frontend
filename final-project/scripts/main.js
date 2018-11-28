ad69c57eea2a33e9a59a399da8eb7a6ab9a44310
Estefi Jacobo <estefania.jacobo@wizeline.com>
2018-11-28 16:44:09 -0600

    refactor main.js

--- final-project/scripts/main.js
+++ final-project/scripts/main.js
@@ -8,16 +8,19 @@ const after = before
   .join("");
 
 var currentBlog = 0;
-var data;
+var blogData;
+var customerInDisplay = 1;
 
 readBlogData();
+addButtonListeners();
+initCustomerSection();
 
 function readBlogData() {
   let url = 'https://wt-4662f45b9eefda7172b747b28d23efdb-0.sandbox.auth0-extend.com/blog';
   fetch(url)
   .then(res => res.json())
   .then((out) => {
-    data = out;
+    blogData = out;
     modifyBlogData(currentBlog);
     sliderColorUpdate(currentBlog);
   })
@@ -28,39 +31,32 @@ function modifyBlogData(currentBlog) {
   var textSection = document.getElementsByClassName('blog-section__content-text');
   var titleText = textSection[0].getElementsByTagName('h1');
   var paragraphText = textSection[0].getElementsByTagName('p');
-  titleText[0].innerHTML = data.articles[currentBlog].title;
-  paragraphText[0].innerHTML = data.articles[currentBlog].description;
+  titleText[0].innerHTML = blogData.articles[currentBlog].title;
+  paragraphText[0].innerHTML = blogData.articles[currentBlog].description;
   var readButton = document.getElementsByClassName("blog-section__btn-read");
   readButton[0].addEventListener('click', function (event) {
-    window.location.replace(data.articles[currentBlog].url);
+    window.location.replace(blogData.articles[currentBlog].url);
   });
   var image = document.getElementsByClassName("blog-section__content-image");
-  image[0].src = data.articles[currentBlog].images.desktop;
+  image[0].src = blogData.articles[currentBlog].images.desktop;
 }
 
-var arrowsBlogSlider = document.querySelector(".blog-section__arrows");
-arrowsBlogSlider.addEventListener("click", updateVisibleBlog, false);
-
-var buttonsBlogSlider = document.querySelector(".blog-section__slider");
-buttonsBlogSlider.addEventListener("click", updateVisibleBlog, false);
+function addButtonListeners() {
+  var arrowsBlogSlider = document.querySelector(".blog-section__arrows");
+  arrowsBlogSlider.addEventListener("click", updateVisibleBlog, false);
+  var buttonsBlogSlider = document.querySelector(".blog-section__slider");
+  buttonsBlogSlider.addEventListener("click", updateVisibleBlog, false);
+}
 
 function updateVisibleBlog(e) {
-    var length = Object.keys(data.articles).length;
+    var length = Object.keys(blogData.articles).length;
     if (e.target !== e.currentTarget) {
       var clickedBlog = e.target.id;
-      if(clickedBlog === 'right') {
-        currentBlog = currentBlog + 1;
-      }
-      else if(clickedBlog === 'left') {
-        currentBlog -= 1
-      }
-      else {
-        currentBlog = clickedBlog
-      }
-      if (currentBlog >= length) {
-        currentBlog = 0;
-      }
-      if (currentBlog < 0) {currentBlog = length-1}
+      if(clickedBlog === 'right') currentBlog += 1;
+      else if(clickedBlog === 'left') currentBlog -= 1;
+      else currentBlog = clickedBlog;
+      if (currentBlog >= length) currentBlog = 0;
+      if (currentBlog < 0) currentBlog = length-1;
       sliderColorUpdate(currentBlog);
       modifyBlogData(currentBlog);
     }
@@ -73,11 +69,15 @@ function sliderColorUpdate(currentSlider) {
     buttons[i].style.backgroundColor = "#D1D6E3";
   }
   buttons[currentSlider].style.backgroundColor = "#5283FF";
-
 }
 
-var buttonsParent = document.querySelector(".customers-section__slider");
-buttonsParent.addEventListener("click", updateCurrentSlide, false);
+function initCustomerSection(){
+  var x = window.matchMedia("(max-width: 400px)");
+  screenWidthChange(x);
+  x.addListener(screenWidthChange);
+  var buttonsParent = document.querySelector(".customers-section__slider");
+  buttonsParent.addEventListener("click", updateCurrentSlide, false);
+}
 
 function updateCurrentSlide(e) {
     if (e.target !== e.currentTarget) {
@@ -86,11 +86,6 @@ function updateCurrentSlide(e) {
     }
     e.stopPropagation();
 }
-
-var customerInDisplay = 1;
-var x = window.matchMedia("(max-width: 400px)");
-screenWidthChange(x);
-x.addListener(screenWidthChange);
 
 function screenWidthChange(x) {
   if (x.matches) {

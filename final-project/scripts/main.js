e9fd580f6a7bce31dee70bfd4429c137efde048e
Daniela Ramón <daniela.perez@wizeline.com>
2019-01-11 00:32:39 -0600

    Add images

--- final-project/scripts/main.js
+++ final-project/scripts/main.js
@@ -14,19 +14,40 @@ async function getArticlesFromUrl(){
 
 function loadCarousel(articles){
   articles.forEach((article, index) => {
-    var slideDiv = document.createElement('div');
-    slideDiv.setAttribute('class', 'blog__content__articles__cards__content__description');
+    // Create image
+    var imageDiv = document.createElement('div');
+    imageDiv.setAttribute('class', 'blog__content__articles__cards__content__image');
+    var image = document.createElement('img');
+    image.setAttribute('src', article.images.desktop);
+    imageDiv.appendChild(image);
+    // Create description
+    var descriptionDiv = document.createElement('div');
+    descriptionDiv.setAttribute('class', 'blog__content__articles__cards__content__description');
     var articleTitle = document.createElement('h3');
     articleTitle.setAttribute('class', 'subtitle');
     articleTitle.innerHTML = article.title;
     var articleContent = document.createElement('p');
     articleContent.setAttribute('class', 'paragraph');
     articleContent.innerHTML = article.description;
-    document.getElementById('blog_container').appendChild(slideDiv);
-    slideDiv.appendChild(articleTitle);
-    slideDiv.appendChild(articleContent);
-
-    //DOTS
+    descriptionDiv.appendChild(articleTitle);
+    descriptionDiv.appendChild(articleContent);
+    // Create buttons
+    var buttonsDiv = document.createElement('div');
+    buttonsDiv.setAttribute('class', 'blog__content__articles__cards__content__description__buttons');
+    var readButton = document.createElement('button');
+    readButton.setAttribute('class', 'button button-blue-pale');
+    readButton.innerHTML = 'Read now';
+    var bookmarkButton = document.createElement('button');
+    bookmarkButton.setAttribute('class', 'button button-bookmarks');
+    bookmarkButton.innerHTML = 'Add to your bookmarks';
+    buttonsDiv.appendChild(readButton);
+    buttonsDiv.appendChild(bookmarkButton);
+    descriptionDiv.appendChild(buttonsDiv);
+    // Add to content div
+    var containerDiv = document.getElementById('blog_container');
+    containerDiv.appendChild(descriptionDiv);
+    containerDiv.appendChild(imageDiv);
+    // Create dots
     var dot = document.createElement('li');
     dot.setAttribute('class', 'carousel-index__dot');
     dot.addEventListener('click', function(){currentArticle(index + 1)}, false);
@@ -36,7 +57,7 @@ function loadCarousel(articles){
     document.getElementById('dots').appendChild(dot);
   });
   showArticles(articlesIndex);
-  // ARROWS
+  // Add event listener to arrows
   var rightArrow = document.getElementById('next-article');
   rightArrow.addEventListener('click', function(){nextArticle(1)}, false);
   var leftArrow = document.getElementById('past-article');
@@ -45,39 +66,36 @@ function loadCarousel(articles){
 
 var articlesIndex = 1;
 
-function nextArticle(n) {
-  showArticles(articlesIndex += n);
+function nextArticle(index) {
+  showArticles(articlesIndex += index);
 }
 
-function currentArticle(n) {
-  showArticles (articlesIndex = n)
+function currentArticle(index) {
+  showArticles (articlesIndex = index)
 }
 
 function showArticles(number) {
-  var slides = document.getElementsByClassName('blog__content__articles__cards__content__description');
+  var articles = document.getElementsByClassName('blog__content__articles__cards__content__description');
+  var images = document.getElementsByClassName('blog__content__articles__cards__content__image');
+  console.log(images);
   var dots = document.getElementsByClassName('far fa-circle');
-  if (number > slides.length) {
+
+  if (number > articles.length) {
     articlesIndex = 1;
   }
   if (number < 1) {
-    articlesIndex = slides.length;
+    articlesIndex = articles.length;
   }
 
-  for (var i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  for (var i = 0; i < articles.length; i++) {
+    articles[i].style.display = 'none';
+    images[i].style.display = 'none';
   }
-  
-  // slides.forEach(slide => {
-  //   slide.style.display = 'none';
-  // });
-
-  for (i = 0; i < dots.length; i++) {
+  for (var i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace('fas fa-circle', '');
   }
-  // dots.forEach(dot => {
-  //   dot.className = dot.className.replace('far fa-circle');
-  // });
 
-  slides[articlesIndex - 1].style.display = 'block';
+  articles[articlesIndex - 1].style.display = 'block';
+  images[articlesIndex - 1].style.display = 'inline-flex';
   dots[articlesIndex - 1].className += ' fas fa-circle';
 }
